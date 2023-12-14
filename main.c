ca094bc048d9761f331026838b2ab79d71b1de5a
Andrei E. Warkentin <andreiw@mm.st>
2023-12-14 10:47:31 -0600

    Successfully link against libusb.
    Signed-off-by: Andrei E. Warkentin <andreiw@mm.st>

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <libusb.h>
 // #include <event2/event.h>
 
 int main(int argc, char **argv)
 {
+  int err;
+  libusb_context *usb_ctx;
+
+  err = libusb_init(&usb_ctx);
+  if (err != 0) {
+    fprintf (stderr, "libusb_init: %s\n", libusb_strerror (err));
+    return 1;
+  }
+
   return 0;
 }
